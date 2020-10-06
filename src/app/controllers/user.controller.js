3dfb2bd24ed55db7b04b67469d1510bd4d66b04b
MarceloTonhetti <marcelotjunior2507@gmail.com>
2020-10-06 19:27:00 -0300

    Criando e implementando rotas de listagens do user

--- src/app/controllers/user.controller.js
+++ src/app/controllers/user.controller.js
@@ -14,5 +14,42 @@ class User {
     })
   }
 
+  viewAllUser(req, res) {
+    user.find({ user_type: "ong" })
+      .sort({ name: 1 })
+      .exec((err, data) => {
+        if (err) {
+          res.status(500).send({ message: "Error processing your request", error: err })
+        } else {
+          if (data.length <= 0) {
+            res.status(200).send({ message: "No users were found in the database" })
+          } else {
+            res.status(200).send({ message: "Successfully recovered all users!", data: data })
+          }
+        }
+      })
+  }
+
+  viewOneUser(req, res) {
+    const { idUser } = req.params
+
+    if (idUser == undefined || idUser == 'null') {
+      res.status(400).send({ message: "The id of the user must be filled in" })
+    }
+
+    user.findOne({ _id: idUser })
+      .exec((err, data) => {
+        if (err) {
+          res.status(500).send({ message: "Error processing your request", error: err })
+        } else {
+          if (data.length <= 0) {
+            res.status(200).send({ message: `User ${idUser} does not exist in the database` })
+          } else {
+            res.status(200).send({ message: `User ${idUser} successfully recovered`, data: data })
+          }
+        }
+      })
+  }
+
 }
 module.exports = new User();
--- src/app/routes/user.routes.js
+++ src/app/routes/user.routes.js
@@ -3,5 +3,7 @@ const route = express.Router()
 const User = require('./../controllers/user.controller')
 
 route.post('/create', User.createUser)
+route.get('/viewAll', User.viewAllUser)
+route.get('/viewOne/:idUser', User.viewOneUser)
 
 module.exports = route
