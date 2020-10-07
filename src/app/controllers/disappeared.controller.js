c5d6d5d9242744c730fb29578f852757f1081eac
Natalia Carmo <nataliatmrs@gmail.com>
2020-10-06 23:26:05 -0300

    Implementando o criar disappeared no controller

---
+++ src/app/controllers/disappeared.controller.js
@@ -0,0 +1,16 @@
+const disappearedschema = require('../../models/disappeared.model')
+
+class Disappeared {
+    createDisappeared(req,res){
+        const body = req.body
+
+        disappearedschema.create(body, (err,data) => {
+            if(err){
+                res.status(500).send({message: "Houve um erro ao processar a sua requisição", error: err})
+            }else{
+                res.status(201).send({message: "Filme criado com sucessso no banco de dados", filme: data})
+            }
+        })
+    }
+}
+module.exports = new Disappeared()
