3a2474d48d10a96aece671c2c06b50096a25fe6f
Natalia Carmo <nataliatmrs@gmail.com>
2020-10-08 11:52:42 -0300

    Disponibilizando a rota de viewAll p/ visual.  Disapp.

--- src/app/routes/disappeared.routes.js
+++ src/app/routes/disappeared.routes.js
@@ -3,5 +3,6 @@ const route = express.Router()
 const Disappeared = require('./../controllers/disappeared.controller')
 
 route.post('/create', Disappeared.createDisappeared)
+route.get('/viewAll', Disappeared.viewAllDisappeareds)
 
 module.exports = route
