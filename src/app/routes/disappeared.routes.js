f7a25de4307fd74320bcf388dd20f1db9d32c4bb
Natalia Carmo <nataliatmrs@gmail.com>
2020-10-08 13:39:51 -0300

    Disponibilizando a rota para visualizar um Disappeared

--- src/app/routes/disappeared.routes.js
+++ src/app/routes/disappeared.routes.js
@@ -4,5 +4,6 @@ const Disappeared = require('./../controllers/disappeared.controller')
 
 route.post('/create', Disappeared.createDisappeared)
 route.get('/viewAll', Disappeared.viewAllDisappeareds)
+route.get('/viewOne/:id', Disappeared.viewOneDisappeared)
 
 module.exports = route
