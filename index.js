f00e919bf53d57c4506aee4b1248d55e81b4eb77
MarceloTonhetti <marcelotjunior2507@gmail.com>
2020-10-29 14:38:24 -0300

    Correção

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const UserRoutes = require('./src/app/routes/user.routes')
 const AdoptionRoutes = require('./src/app/routes/adoption.routes')
 const AdRoutes = require('./src/app/routes/ad.routes')
 const DisappearedRoutes = require('./src/app/routes/disappeared.routes')
+const SpeciesRoutes = require('./src/app/routes/species.routes')
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.text())
@@ -17,7 +18,7 @@ app.use(bodyParser.json({ type: 'application/json' }))
 app.use(cors())
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', "")
+  res.header('Access-Control-Allow-Origin', "*")
   res.header('Access-Control-Allow-Header', "Origin, X-Requested-With, Content-Type, Accept")
   next()
 })
@@ -31,6 +32,7 @@ app.use('/user', UserRoutes)
 app.use('/adoption', AdoptionRoutes)
 app.use('/ad', AdRoutes)
 app.use('/disappeared', DisappearedRoutes)
+app.use('/species', SpeciesRoutes)
 
 
 app.use('*', (req, res) => {
