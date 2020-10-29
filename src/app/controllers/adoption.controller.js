5e96d7b336b27ac5c57bc248fa81ef2dbdbd9924
MarceloTonhetti <marcelotjunior2507@gmail.com>
2020-10-29 14:38:24 -0300

    Correção

--- src/app/controllers/adoption.controller.js
+++ src/app/controllers/adoption.controller.js
@@ -3,9 +3,9 @@ const user = require('./../controllers/user.controller')
 
 class Adoption {
     createAdoption(req, res) {
-        const reqBody = req.body
+        const body = req.body
 
-        adoption.create(reqBody, (err, data) => {
+        adoption.create(body, (err, data) => {
             if (err) {
                 res.status(500).send({ message: 'Error processing your request', error: err })
             } else {
@@ -51,7 +51,7 @@ class Adoption {
                     if (data == null) {
                         res.status(200).send({ message: `Adoption does not exist in the database` })
                     } else {
-                        res.status(200).send({ message: `Adoption ${data.species} successfully recovered`, data: data })
+                        res.status(200).send({ message: `Adoption ${data.name} successfully recovered`, data: data })
                     }
                 }
             })
@@ -60,27 +60,27 @@ class Adoption {
     updateAdoption(req, res) {
         const { adoptionId } = req.params
         const reqBody = req.body
-    
-        adoption.updateOne({ _id: adoptionId },  { $set: {"rated":true}}, (err, adoption) => {
-          if (err) {
-            res.status(500).send({ message: 'Error processing your request'})
-          } else {
-            res.status(200).send({ message: 'adoption was successfully updated, data: adoption' })
-          }
-        })
-      }
 
-      deleteAdoption(req, res) {
-        const { adoptionId } = req.params
-    
-        adoption.deleteOne({ _id: adoptionId }, (err, result) => {
-          if (err) {
-            res.status(500).send({ message: 'Error processing your request' })
-          } else {
-            res.status(200).send({ message: 'adoption successfully deleted, data: result' })
-          }
+        adoption.updateOne({ _id: adoptionId }, { $set: { "rated": true } }, (err, adoption) => {
+            if (err) {
+                res.status(500).send({ message: 'Error processing your request' })
+            } else {
+                res.status(200).send({ message: 'adoption was successfully updated, data: adoption' })
+            }
         })
-      }  
+    }
+
+    deleteAdoption(req, res) {
+        const { adoptionId } = req.params
+
+        adoption.deleteOne({ _id: adoptionId }, (err, result) => {
+            if (err) {
+                res.status(500).send({ message: 'Error processing your request' })
+            } else {
+                res.status(200).send({ message: 'adoption successfully deleted, data: result' })
+            }
+        })
+    }
 
 
 }
