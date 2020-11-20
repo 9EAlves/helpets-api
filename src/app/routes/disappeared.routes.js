9ea7fd9ee2e75be8cf67dc5a6f81c57f114245fe
unknown <9ealves@gmail.com>
2020-11-20 15:06:17 -0300

    rated e tals

--- src/app/routes/disappeared.routes.js
+++ src/app/routes/disappeared.routes.js
@@ -4,6 +4,6 @@ const Disappeared = require('./../controllers/disappeared.controller')
 
 route.post('/create', Disappeared.createDisappeared)
 route.get('/viewAll', Disappeared.viewAllDisappeareds)
-route.get('/viewOne/:id', Disappeared.viewOneDisappeared)
+route.get('/viewOne/:disappearedId', Disappeared.viewOneDisappeared)
 
 module.exports = route
