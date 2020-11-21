27bd4f10b12cde85f7be9ca447fc15575e1e8fd5
MarceloTonhetti <marcelotjunior2507@gmail.com>
2020-11-21 17:16:26 -0300

    Corrigindo importações

--- src/app/routes/public/routes/disappeared.routes.js
+++ src/app/routes/public/routes/disappeared.routes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const route = express.Router()
-const Disappeared = require('./../controllers/disappeared.controller')
+const Disappeared = require('./../../../controllers/disappeared.controller')
 
 route.get('/viewAll', Disappeared.viewAllDisappeareds)
 route.get('/viewOne/:disappearedId', Disappeared.viewOneDisappeared)
