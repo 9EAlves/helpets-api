e5f12dc089a4979fc6342d872145f6a177b091d4
MarceloTonhetti <marcelotjunior2507@gmail.com>
2020-11-21 16:12:04 -0300

    Separando as rotas em public e private

--- src/app/routes/species.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const route = express.Router()
-const Specie = require('./../controllers/species.controller')
-
-route.post('/create', Specie.createSpecie)
-route.get('/viewAll', Specie.viewAllSpecies)
-
-module.exports = route
