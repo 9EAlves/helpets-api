9d15b710603bac52d0e5b56a2561f746f4e779a4
MarceloTonhetti <marcelotjunior2507@gmail.com>
2020-11-21 16:12:04 -0300

    Separando as rotas em public e private

--- src/app/routes/private/routes/species.routes.js
+++ src/app/routes/private/routes/species.routes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const route = express.Router()
-const Specie = require('./../controllers/species.controller')
+const Specie = require('./../../../controllers/species.controller')
 
 route.post('/create', Specie.createSpecie)
 route.get('/viewAll', Specie.viewAllSpecies)
--- src/app/routes/public/routes/disappeared.routes.js
+++ src/app/routes/public/routes/disappeared.routes.js
@@ -2,7 +2,6 @@ const express = require('express')
 const route = express.Router()
 const Disappeared = require('./../controllers/disappeared.controller')
 
-route.post('/create', Disappeared.createDisappeared)
 route.get('/viewAll', Disappeared.viewAllDisappeareds)
 route.get('/viewOne/:disappearedId', Disappeared.viewOneDisappeared)
 
