190dfd4b8da6bdf88acaa848657240ae64dccb2e
MarceloTonhetti <marcelotjunior2507@gmail.com>
2020-11-21 16:12:04 -0300

    Separando as rotas em public e private

--- src/app/routes/private/routes/adoption.routes.js
+++ src/app/routes/private/routes/adoption.routes.js
@@ -4,9 +4,7 @@ const Adoption = require('./../../../controllers/adoption.controller')
 
 
 route.post('/create', Adoption.createAdoption)
-route.get('/viewAll', Adoption.getWithParams)
+route.get('/viewAll', Adoption.viewAllAdoption)
 route.get('/viewOne/:adoptionId', Adoption.viewOneAdoption)
-route.put('/update/:adoptionId', Adoption.updateAdoption)
-route.delete('/delete/:adoptionId', Adoption.deleteAdoption)
 
 module.exports = route
--- src/app/routes/public/routes/disappeared.routes.js
+++ src/app/routes/public/routes/disappeared.routes.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const route = express.Router()
-const Disappeared = require('./../../../controllers/disappeared.controller')
+const Disappeared = require('./../controllers/disappeared.controller')
 
-route.get('/viewAll', Disappeared.getWithParams)
+route.get('/viewAll', Disappeared.viewAllDisappeareds)
 route.get('/viewOne/:disappearedId', Disappeared.viewOneDisappeared)
 
 module.exports = route
