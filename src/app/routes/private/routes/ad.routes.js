125a124510ba675ef51dd22850564d1980390600
MarceloTonhetti <marcelotjunior2507@gmail.com>
2020-11-21 17:16:26 -0300

    Corrigindo importações

--- src/app/routes/private/routes/ad.routes.js
+++ src/app/routes/private/routes/ad.routes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const route = express.Router()
-const Ad = require('./../.././controllers/ad.controller')
+const Ad = require('./../../../controllers/ad.controller')
 
 route.post('/create', Ad.createAd)
 route.get('/viewAll', Ad.viewallAd)
