a5163941dc03a7ba40dbd25dfad6c001c6ce5230
MarceloTonhetti <marcelotjunior2507@gmail.com>
2020-12-04 04:05:53 -0300

    Adicionando rotas de update e delete de disappeared e adoptions

--- src/app/routes/private/routes/disappeared.routes.js
+++ src/app/routes/private/routes/disappeared.routes.js
@@ -5,5 +5,7 @@ const Disappeared = require('./../../../controllers/disappeared.controller')
 route.post('/create', Disappeared.createDisappeared)
 route.get('/viewAll', Disappeared.getWithParams)
 route.get('/viewOne/:disappearedId', Disappeared.viewOneDisappeared)
+route.put('/update/:disappearedId', Disappeared.updateDisappeared)
+route.delete('/delete/:disappearedId', Disappeared.deleteDisappeared)
 
 module.exports = route
